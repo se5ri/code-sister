9808d48f78294acff0bd87f29b4832dcf3f5eb4d
오서현 <bbibbi1118@naver.com>
2025-06-19 22:57:28 +0900

    ✨ 변수

---
+++ JAVASCRIPT/variable.js
@@ -0,0 +1,71 @@
+/* let color = "pink";
+color = "black";
+
+console.log(color);
+
+// const는 재할당이 안됨 (고정적인 값에 써줌)
+const password = "pink";
+console.log(password); */
+
+// var 쓰지마..
+
+// 변수에 들어갈 수 있는거:
+/*
+string(문자열): "" 또는 '' 안에 있는 문자 
+number: 숫자
+boolean: true와 false
+...
+ */
+
+// 논리연산자
+// ||: 하나만 참이여도 true
+/* let password = 2;
+password = password > 3 || password > 1; // true
+
+console.log(password); */
+
+// &&: 둘다 참이면 true (하나라도 false면 false)
+// let password = 2;
+// password = password > 3 && password > 1; // false
+
+// console.log(password);
+
+// 문제 1
+/* let a = 1;
+let b = 2;
+console.log(a, b);
+
+let c = a; //1
+a = b; // 2
+b = c; //1
+console.log(a, b); */
+
+// 20 + 30;
+//console.log(20 + 30); // 50
+
+// "20" + "30"; //2030
+// console.log("20" + "30");
+
+// "Hello" + " " + 2021; //Hello 2021
+// console.log("Hello" + " " + 2021);
+
+// 1 + 2 * 3 * (1 + 3) ** 2;
+// console.log(1 + 2 * 3 * (1 + 3) ** 2);
+// 1 / 0;
+// 6 % 2;
+// 7.5 % 2;
+// 5 == 5;
+// 5 === 5;
+// 5 == "5";
+// 5 === "5";
+// 5 != 5.0;
+// 5 !== 5.0;
+// "true" === true;
+// 5 <= 5.0;
+// 5 >= 5;
+// true || true;
+// true || false;
+// true && true;
+// true && false;
+// !true;
+// !false;
